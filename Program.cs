46e566d79a3159d0a31cd68097f38a8558cae749
emmas97 <78239177+emmas97@users.noreply.github.com>
2021-05-10 12:08:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			< !DOCTYPE html >
+ < html >
+ < body >
+ 
+
+
+ < h1 > Customer Information </ h1 >
+	< form action = "/action_page.php" >
+	 
+		 < label for= "fname" > First name:</ label >
+	  
+		  < input type = "text" id = "fname" name = "fname" >< br >< br >
+	  
+		  < label for= "lname" > Last name:</ label >
+	   
+		   < input type = "text" id = "lname" name = "lname" >< br >< br >
+	   
+		   < label for= "email" > E - mail:</ label >
+		
+			< input type = "text" id = "email" name = "email" >< br >< br >
+		
+
+		< button type = "button" > Submit </ button >
+		
+
+
+
+		</ form >
+
+		}
+    }
+}
